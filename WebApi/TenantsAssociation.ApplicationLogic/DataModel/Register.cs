b25f365860c4eb4817d9ec7286906529925c9b2c
Dragos <tolgoi.dragos@gmail.com>
2020-04-27 14:39:53 +0300

    Login-backend-update

---
+++ WebApi/TenantsAssociation.ApplicationLogic/DataModel/Register.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TenantsAssociation.ApplicationLogic.DataModel
+{
+    public class Register
+    {
+        public string Username { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- WebApi/TenantsAssociation/Startup.cs
+++ WebApi/TenantsAssociation/Startup.cs
@@ -46,6 +46,7 @@ namespace TenantsAssociation
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<UserService>();
             services.AddScoped<InvoiceService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
